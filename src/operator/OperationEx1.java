43d0d39fa6f4ed35336ac926bcb1886324475451
bjmin17 <bjmin17@naver.com>
2019-08-26 19:06:28 +0900

    First

---
+++ src/operator/OperationEx1.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class OperationEx1 {
+
+	public static void main(String[] args) {
+		int mathScore = 90;
+		int engScore = 70;
+		int korScore = 80;
+		
+		int totalScore = mathScore + engScore; //총점 구하기
+		System.out.println(totalScore);
+		
+		double avgScore = totalScore / 2.0; // 평균 구하기
+		System.out.println(avgScore);
+		
+		totalScore = korScore + mathScore + engScore;
+		System.out.println(totalScore);
+		
+		avgScore = totalScore / 3.0;
+		System.out.println(avgScore);
+		
+	}
+
+}
